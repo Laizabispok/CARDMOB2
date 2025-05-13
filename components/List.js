eb72eb698434320daca8c4143a082bc19c3d0e4f
LAIZA VITORIA DA SILVA BISPO <CG3026655@car.ifsp>
2025-05-13 07:49:49 -0300

    Add List component [done]

--- components/List.js
+++ components/List.js
@@ -1,16 +1,42 @@
 import React, { Component } from "react";
-import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+
 
 class List extends Component {
     state = {
         names: [
-            { id: 0, name: 'Ben'},
-            { id: 1, name: 'Susan'},
-            { id: 2, name: 'Robert'},
-            { id: 3, name: 'Chrischarles'}
-        ],
-    };
-  
+            {id: 0, name: 'Ben'},
+            {id: 1, name: 'Susan'},
+            {id: 2, name: 'Roberth'},
+            {id: 3, name: 'Chrischarles'}
+        ]
+    }
+    alertItemName = (item) => { 
+        alert(item.name);
+    }
+
+    render() {
+        return (
+            <View>
+                <Text style={styles.text}>
+                    Lista de itens "clicáveis"
+                </Text>
+                {
+                this.state.names.map((item, index) => (
+                <TouchableOpacity
+                    key={item.id}
+                style={styles.container}
+                onPress={() => this.alertItemName(item)}
+                >
+                    <Text style={styles.text}>
+                        {item.name}
+                    </Text>
+                </TouchableOpacity>
+                ))
+             }
+            </View>
+        );
+    }
 }
 export default List;
 
@@ -18,10 +44,10 @@ const styles = StyleSheet.create({
     container: {
         padding: 10,
         marginTop: 3,
-        backgroundColor: "#d9f9b1",
-        alignItems: "center",
+        backgroundColor: '#d9f9b1',
+        alignItems: 'center',
     },
     text: {
-       color: "#4f603c",
-    },
-    });
+        color: '#4f603c',
+    }
+});
